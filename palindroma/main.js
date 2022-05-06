375eac4a8c65ab348b4e8bb1bc8146447176773c
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-05-07 00:30:27 +0200

    first commit

---
+++ palindroma/main.js
@@ -0,0 +1,9 @@
+//chiedere all'utente di inserire una parola 
+// fare un check tramite una funzione, che definirà se è palindroma o no
+
+//creo una funzione 
+//salvo all'interno la lunghezza del mio imput
+//ciclo per metà della lunghezza del mio imput
+//controllo che entrambe le metà siano uguali 
+// assegno la mia funzione alla costante imput
+
