64f98994d39ca9cc1fab882b75db6f73609c2151
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-05-07 00:49:30 +0200

    logica fatta non funzionante

--- palindroma/main.js
+++ palindroma/main.js
@@ -1,9 +1,19 @@
 //chiedere all'utente di inserire una parola 
 // fare un check tramite una funzione, che definirà se è palindroma o no
-
+//salvo in una costante la parola chiesta
 //creo una funzione 
-//salvo all'interno la lunghezza del mio imput
-//ciclo per metà della lunghezza del mio imput
-//controllo che entrambe le metà siano uguali 
-// assegno la mia funzione alla costante imput
-
+const word = prompt("inserisci una parola");
+function palidromeChek(word){
+   //salvo all'interno la lunghezza del mio imput
+   const Wordlenght = word.length;
+   //ciclo per metà della lunghezza del mio imput
+   for(let i = 0; i < Wordlenght / 2; i++){
+      //controllo che entrambe le metà siano uguali 
+      if(word[i] == word[Wordlenght - 1 - i]){
+         return "la parola è palindroma"
+      }
+   }
+   return "la parola non è palindroma"
+}
+//chiamo la funzione
+const check = palidromeChek(word)
