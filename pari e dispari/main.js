966fbbb2e46b5b37e9fe4a812234dff8aea75b94
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-05-06 14:50:38 +0200

    first commit pari e dispari

--- pari e dispari/main.js
+++ pari e dispari/main.js
@@ -1,7 +1,25 @@
+
+//__________________________________
+//
+//               main
+//___________________________________
+//chiediamo all'utente se vuole pari o disparo 
+//asseconda della sua scelta gli chiediamo di inserire un numero pari o dispari tra 1 e 5 
+const Sutente = prompt("scegli pari o dispari");
+const Nutente = Number(prompt("inserisci un numero compreso tra 1 e 5 asseconda della tua scelta precedente"));
+
+let Ncomp = getRandomNumber (1, 5);
+console.log(Ncomp)
+
+
+
+
+
+
 //__________________________________
 
-             FUNZIONI
-_________________________________//  
+//            FUNZIONI
+//_________________________________//  
 // pari o dispri    
 function isEven(num) {
    if( num % 2 === 0 ) {
@@ -25,7 +43,3 @@ function getRandomNumbersList(nElements, min, max) {
    }
    return numbers;
 }
-//__________________________________
-//
-//               main
-//___________________________________
